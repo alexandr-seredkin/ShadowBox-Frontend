9bb4945f319538bbc4c38d7f13ffa0551acc5e80
Alexandr Seredkin <seredkin@dvg-project.com>
2024-10-13 14:33:18 +0300

    scirpt tag settings change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
+
   variable: "--font-geist-sans",
   weight: "100 900",
 });
@@ -28,33 +29,38 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
+          async
+          strategy="lazyOnload"
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
           src="/nonexistent/angular.js/1.8.3/angular.min.js"
         ></Script>
         <Script
+          async
+          strategy="lazyOnload"
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
           src="/nonexistent/vue.js/3.1.3/vue.min.js"
         ></Script>
         <Script
+          async
+          strategy="lazyOnload"
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
           src="/nonexistent/wp-includes/js/jquery.js/3.6.0/jquery.min.js"
         />
         <Script
+          async
+          strategy="lazyOnload"
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
           src="/nonexistent/wp-content/themes/twentytwentyone/assets/js/global.js"
         />
         <Script
+          async
+          strategy="lazyOnload"
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
           src="/nonexistent/wp-content/plugins/contact-form-7/includes/js/scripts.js"
         />
       </head>
