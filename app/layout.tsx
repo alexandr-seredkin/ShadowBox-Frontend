4d03b88b7e952e42339f1285b6759e144265f7a4
Alexandr Seredkin <seredkin@dvg-project.com>
2024-10-13 14:53:05 +0300

    script change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,41 +27,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
-          async
-          strategy="lazyOnload"
+        <script
+          defer
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
           src="/nonexistent/angular.js/1.8.3/angular.min.js"
-        ></Script>
-        <Script
-          async
-          strategy="lazyOnload"
+        ></script>
+        <script
+          defer
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
           src="/nonexistent/vue.js/3.1.3/vue.min.js"
-        ></Script>
-        <Script
-          async
-          strategy="lazyOnload"
+        ></script>
+        <script
+          defer
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
           src="/nonexistent/wp-includes/js/jquery.js/3.6.0/jquery.min.js"
-        />
-        <Script
-          async
-          strategy="lazyOnload"
+        ></script>
+        <script
+          defer
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
           src="/nonexistent/wp-content/themes/twentytwentyone/assets/js/global.js"
-        />
-        <Script
-          async
-          strategy="lazyOnload"
+        ></script>
+        <script
+          defer
           integrity="sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
           crossOrigin="anonymous"
           src="/nonexistent/wp-content/plugins/contact-form-7/includes/js/scripts.js"
-        />
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
